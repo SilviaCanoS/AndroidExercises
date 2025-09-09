21e369ee7ee358b33d766fadf85ddc574276bb50
SilviaCanoS <silviacanosarabia@gmail.com>
2025-09-09 11:57:01 -0600

    Delete Rick and Morty

--- RecyclerViewRickAndMorty/Actividad1/app/src/main/java/com/solera/actividad1/RickAndMortyService.kt
+++
@@ -1,9 +0,0 @@
-package com.solera.actividad1
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface RickAndMortyService {
-    @GET("character")
-    fun getCharacters():Call<CharacterResponse>
-}
